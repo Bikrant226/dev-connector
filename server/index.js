7015368aa0062c39a166981a4113ae02b7c36791
Bikrant226 <bikrantthakur6@gmail.com>
2023-09-16 21:37:23 +0530

    Initial file added

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('Running!!')
+})
+const PORT=process.env.PORT || 3001
+
+app.listen(PORT,()=>
+    console.log(`Server started at ${PORT}`)
+);
